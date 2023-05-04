4703a48b412f3f08ad9cd95b1584546a5e2777e5
beyart <98033010+beyart@users.noreply.github.com>
2023-05-04 20:04:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -43,3 +43,30 @@ function autoScroll() {
   }, 5000);
 }
 
+// get all images in the gallery
+const images = document.querySelectorAll('.Gallery-card-img');
+
+// add a click event listener to each image
+images.forEach(image => {
+  image.addEventListener('click', enlargeImage);
+});
+
+// function to enlarge the clicked image
+function enlargeImage(event) {
+  // create a new image element
+  const enlargedImage = new Image();
+
+  // set the src of the new image element to the src of the clicked image
+  enlargedImage.src = event.target.src;
+
+  // set the class of the new image element to 'enlarged-image'
+  enlargedImage.classList.add('enlarged-image');
+
+  // add the new image element to the document
+  document.body.appendChild(enlargedImage);
+
+  // add a click event listener to the new image element to remove it when clicked
+  enlargedImage.addEventListener('click', () => {
+    document.body.removeChild(enlargedImage);
+  });
+}
